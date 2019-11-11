fbed33a44d32e9b2f76c30bdb6e5e05f1b85e76a
otajisan <taji.taji.otajisan@gmail.com>
2019-11-12 06:28:46 +0900

    :shirt: apply Kotlin Style Guide

--- src/main/kotlin/io/code/morning/ApiAppCdk.kt
+++ src/main/kotlin/io/code/morning/ApiAppCdk.kt
@@ -5,18 +5,18 @@ import software.amazon.awscdk.core.Environment
 import software.amazon.awscdk.core.StackProps
 
 fun main() {
-    val app = App()
-    ApiStack(
-        app, "morning-code-api",
-        StackProps.builder()
-            .env(
-                Environment.builder()
-                    .region("ap-northeast-1")
-                    .build()
-            )
-            .build()
-    )
+  val app = App()
+  ApiStack(
+    app, "morning-code-api",
+    StackProps.builder()
+      .env(
+        Environment.builder()
+          .region("ap-northeast-1")
+          .build()
+      )
+      .build()
+  )
 
-    app.synth()
+  app.synth()
 
 }
--- src/main/kotlin/io/code/morning/ApiStack.kt
+++ src/main/kotlin/io/code/morning/ApiStack.kt
@@ -9,44 +9,44 @@ import software.amazon.awscdk.services.ecr.Repository
 import software.amazon.awscdk.services.ecs.*
 
 class ApiStack @JvmOverloads constructor(app: App, id: String, props: StackProps? = null) :
-    Stack(app, id, props) {
+  Stack(app, id, props) {
 
-    init {
-        // VPC
-        val vpc = Vpc(
-            this, "morning-code-api-vpc", VpcProps.builder()
-                .cidr("10.0.0.0/16")
-                .build()
-        )
+  init {
+    // VPC
+    val vpc = Vpc(
+      this, "morning-code-api-vpc", VpcProps.builder()
+        .cidr("10.0.0.0/16")
+        .build()
+    )
 
-        // ECS Cluster
-        val ecsCluster = Cluster(
-            this, "morning-code-api-cluster", ClusterProps.builder()
-                .vpc(vpc)
-                .build()
-        )
+    // ECS Cluster
+    val ecsCluster = Cluster(
+      this, "morning-code-api-cluster", ClusterProps.builder()
+        .vpc(vpc)
+        .build()
+    )
 
-        // ECR
-        val ecr = Repository(this, "morning-code-api")
+    // ECR
+    val ecr = Repository(this, "morning-code-api")
 
-        // TaskDefinition
-        val taskDefinition =
-            FargateTaskDefinition(
-                this, "morning-code-api-fargate-task-definition",
-                FargateTaskDefinitionProps.builder().build()
-            )
-                .addContainer(
-                    "morning-code-api-container",
-                    ContainerDefinitionOptions.builder().image(ContainerImage.fromEcrRepository(ecr)).build()
-                ).taskDefinition
+    // TaskDefinition
+    val taskDefinition =
+      FargateTaskDefinition(
+        this, "morning-code-api-fargate-task-definition",
+        FargateTaskDefinitionProps.builder().build()
+      )
+        .addContainer(
+          "morning-code-api-container",
+          ContainerDefinitionOptions.builder().image(ContainerImage.fromEcrRepository(ecr)).build()
+        ).taskDefinition
 
-        // Fargate
-        FargateService(
-            this, "morning-code-api-fargate", FargateServiceProps.builder()
-                .cluster(ecsCluster)
-                .desiredCount(1)
-                .taskDefinition(taskDefinition)
-                .build()
-        )
-    }
+    // Fargate
+    FargateService(
+      this, "morning-code-api-fargate", FargateServiceProps.builder()
+        .cluster(ecsCluster)
+        .desiredCount(1)
+        .taskDefinition(taskDefinition)
+        .build()
+    )
+  }
 }
